7a0ac49480e9ece4726f6823fc91ff65c195cbab
fieldstyler <tdfields93@gmail.com>
2020-08-02 14:04:25 -0600

    Add eighty_characters_per_line method that prints each line with up to 80 characters. Needs refactoring BADLY

--- lib/printer.rb
+++ lib/printer.rb
@@ -4,10 +4,7 @@ class Printer
   def initialize(input)
     @dictionary = Dictionary.new
     @input = input
-    @top = ''
-    @mid = ''
-    @bot = ''
-
+    @output = []
   end
 
   def separate_input
@@ -21,24 +18,49 @@ class Printer
   end
 
   def top_line
-    translate_input_to_braille.reduce("") do |line, braille_letter|
-      line << braille_letter[0]
+    translate_input_to_braille.reduce("") do |line, braille_character|
+      line << braille_character[0]
     end
   end
 
   def middle_line
-    translate_input_to_braille.reduce("") do |line, braille_letter|
-      line << braille_letter[1]
+    translate_input_to_braille.reduce("") do |line, braille_character|
+      line << braille_character[1]
     end
   end
 
   def bottom_line
-    translate_input_to_braille.reduce("") do |line, braille_letter|
-      line << braille_letter[2]
+    translate_input_to_braille.reduce("") do |line, braille_character|
+      line << braille_character[2]
     end
   end
 
-  def print_translation
-
+  def eighty_characters_per_line
+    top = top_line
+    mid = middle_line
+    bot = bottom_line
+    if top.length <= 80
+      @output << top_line.slice!(0..79) + "\n"
+      @output << middle_line.slice!(0..79) + "\n"
+      @output << bottom_line.slice!(0..79)
+    elsif (top.length > 80 && top.length <= 160)
+      @output << top_line.slice!(0..79) + "\n"
+      @output << middle_line.slice!(0..79) + "\n"
+      @output << bottom_line.slice!(0..79) + "\n"
+      @output << top_line.slice!(80..159) + "\n"
+      @output << middle_line.slice!(80..159) + "\n"
+      @output << bottom_line.slice!(80..159)
+    elsif (top.length > 160 && top.length <= 240)
+      @output << top_line.slice!(0..79) + "\n"
+      @output << middle_line.slice!(0..79) + "\n"
+      @output << bottom_line.slice!(0..79) + "\n"
+      @output << top_line.slice!(80..159) + "\n"
+      @output << middle_line.slice!(80..159) + "\n"
+      @output << bottom_line.slice!(80..159) + "\n"
+      @output << top_line.slice!(160..239) + "\n"
+      @output << middle_line.slice!(160..239) + "\n"
+      @output << bottom_line.slice!(160..239)
+    end
+      @output.join
   end
 end
