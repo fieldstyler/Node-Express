fd2acb9d36f2b560b54d06b4262464249411ed17
fieldstyler <tdfields93@gmail.com>
2020-08-01 20:48:45 -0600

    Add necessary requires and initial test_it_exists for night_writer file

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,1 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./night_writer"
+require "./lib/file_handler"
 
+class NightWriterTest < Minitest::Test
+
+  def test_it_exists
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    assert_instance_of NightWriter, night_writer
+  end
+
+end
