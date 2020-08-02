3ca4887cca6a95c2bc5e6b4d80a0e3e368859433
fieldstyler <tdfields93@gmail.com>
2020-08-02 13:00:12 -0600

    Add methods that translate english to braille and separate into top, middle, and bottom lines

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,20 +1,41 @@
 require "./lib/dictionary"
 class Printer
 
-  def initialize
+  def initialize(input)
+    @dictionary = Dictionary.new
+    @input = input
+    @top = ''
+    @mid = ''
+    @bot = ''
 
+  end
+
+  def separate_input
+    @input.chars
+  end
+
+  def translate_input_to_braille
+    separate_input.map do |input|
+      @dictionary.all_chars[input]
+    end
   end
 
   def top_line
-
+    translate_input_to_braille.reduce("") do |line, braille_letter|
+      line << braille_letter[0]
+    end
   end
 
   def middle_line
-
+    translate_input_to_braille.reduce("") do |line, braille_letter|
+      line << braille_letter[1]
+    end
   end
 
   def bottom_line
-
+    translate_input_to_braille.reduce("") do |line, braille_letter|
+      line << braille_letter[2]
+    end
   end
 
   def print_translation
