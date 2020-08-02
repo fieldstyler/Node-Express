5504abb8a4d3c1a92dbe62ff0b935c98fbcc210c
fieldstyler <tdfields93@gmail.com>
2020-08-01 20:55:38 -0600

    Add new test that reads info from english.txt and copy-pastes the text into the braille.txt file

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -11,4 +11,9 @@ class NightWriterTest < Minitest::Test
     assert_instance_of NightWriter, night_writer
   end
 
+  def test_it_has_attributes
+    night_writer = NightWriter.new("message.txt", "braille.txt")
+    assert_equal "Created braille.txt containing 16 characters.", night_writer.translate_english_to_braille
+  end
+
 end
