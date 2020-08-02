4158b28d92b5e1380292875fe8de57d7989e3a05
fieldstyler <tdfields93@gmail.com>
2020-08-02 11:11:28 -0600

    Create printer class with empty methods

---
+++ lib/printer.rb
@@ -0,0 +1,23 @@
+require "./lib/dictionary"
+class Printer
+
+  def initialize
+
+  end
+
+  def top_line
+
+  end
+
+  def middle_line
+
+  end
+
+  def bottom_line
+
+  end
+
+  def print_translation
+
+  end
+end
