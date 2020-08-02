450b58c9d2906163e6946657e9246907bbe73074
fieldstyler <tdfields93@gmail.com>
2020-08-02 10:29:34 -0600

    Add test_number_to_braille test that translates numbers to braille

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -9,9 +9,16 @@ class DictionaryTest < Minitest::Test
     assert_instance_of Dictionary, dictionary
   end
 
-  def test_english_to_braille_lowercase
+  def test_english_to_braille_lowercase_letter
     dictionary = Dictionary.new
     assert_equal ["0.", "..", ".."], dictionary.letters["a"]
   end
+
+  def test_number_to_braille
+    dictionary = Dictionary.new
+    assert_equal ['00', '.0', '..'], dictionary.numbers["4"]
+  end
+
+
 
 end
