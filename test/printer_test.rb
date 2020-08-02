716a7f53094c3c785e888ad02f894f99dc2d79b0
fieldstyler <tdfields93@gmail.com>
2020-08-02 12:59:35 -0600

    Add tests for printing top, middle, and bottom lines

--- test/printer_test.rb
+++ test/printer_test.rb
@@ -6,13 +6,50 @@ require "./lib/printer"
 class PrinterTest < Minitest::Test
 
   def test_it_exists
-    printer = Printer.new
+    printer = Printer.new('a')
     assert_instance_of Printer, printer
   end
 
-  def test_can_print_characters_in_rows
-    printer = Printer.new
-    
+  def test_can_separate_characters_in_input
+    printer = Printer.new('a')
+    assert_equal ['a'], printer.separate_input
+    printer1 = Printer.new('ab')
+    assert_equal ['a', 'b'], printer1.separate_input
+  end
+
+  def test_can_translate_letters_to_braille_given_string
+    printer = Printer.new('a')
+    assert_equal [['0.', '..', '..']], printer.translate_input_to_braille
+    printer1 = Printer.new('ab')
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], printer1.translate_input_to_braille
+  end
+
+  def test_can_return_top_line
+    printer1 = Printer.new('ab')
+    assert_equal "0.0.", printer1.top_line
+    printer2 = Printer.new('2a')
+    assert_equal "0.0.", printer2.top_line
+    printer3 = Printer.new('7A!')
+    assert_equal "00..0...", printer3.top_line
+
+  end
+
+  def test_can_return_middle_line
+    printer1 = Printer.new('ab')
+    assert_equal "..0.", printer1.middle_line
+    printer2 = Printer.new('2a')
+    assert_equal "0...", printer2.middle_line
+    printer3 = Printer.new('7A!')
+    assert_equal "00....00", printer3.middle_line
+  end
+
+  def test_can_return_bottom_line
+    printer1 = Printer.new('ab')
+    assert_equal "....", printer1.bottom_line
+    printer2 = Printer.new('2k')
+    assert_equal "..0.", printer2.bottom_line
+    printer3 = Printer.new('7A!')
+    assert_equal "...0..0.", printer3.bottom_line
   end
 
 end
