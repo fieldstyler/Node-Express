4049aaaae04dc35f6fd9e727195f32557cc0bc54
fieldstyler <tdfields93@gmail.com>
2020-08-03 12:20:45 -0600

    Create method to translate file into output file

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -8,4 +8,14 @@ module FileHandler
     end
     "Created #{output} containing #{File.read(output).chomp.length} characters."
   end
+
+  def self.read_and_translate_file(input, output)
+
+    contents = File.read(input)
+    printer = Printer.new(contents)
+    File.open(output, "w") do |file|
+      file.write printer.translate_to_braille_max_eighty_chars
+    end
+  end
+
 end
