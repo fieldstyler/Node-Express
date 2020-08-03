c6f5cb2e83cdc9de884b757e0c4d27274e61988b
fieldstyler <tdfields93@gmail.com>
2020-08-03 13:37:49 -0600

    Update to remove numbers from tests and add new braille_to_english test that passes

--- test/printer_test.rb
+++ test/printer_test.rb
@@ -12,45 +12,38 @@ class PrinterTest < Minitest::Test
 
   def test_can_separate_characters_in_input
     printer = Printer.new('a')
-    assert_equal ['a'], printer.separate_input
+    assert_equal ['a'], printer.separate_english_characters
     printer1 = Printer.new('ab')
-    assert_equal ['a', 'b'], printer1.separate_input
+    assert_equal ['a', 'b'], printer1.separate_english_characters
   end
 
   def test_can_translate_letters_to_braille_given_string
     printer = Printer.new('a')
-    assert_equal [['0.', '..', '..']], printer.translate_input_to_braille
+    assert_equal [['0.', '..', '..']], printer.translate_english_to_braille
     printer1 = Printer.new('ab')
-    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], printer1.translate_input_to_braille
+    assert_equal [["0.", "..", ".."], ["0.", "0.", ".."]], printer1.translate_english_to_braille
   end
 
   def test_can_return_top_line
     printer1 = Printer.new('ab')
     assert_equal "0.0.", printer1.top_line
-    printer2 = Printer.new('2a')
-    assert_equal "0.0.", printer2.top_line
-    printer3 = Printer.new('7A!')
-    assert_equal "00..0...", printer3.top_line
-
+    printer2 = Printer.new('FoSho!')
+    assert_equal "..000....00.0...", printer2.top_line
   end
 
   def test_can_return_middle_line
     printer1 = Printer.new('ab')
     assert_equal "..0.", printer1.middle_line
-    printer2 = Printer.new('2a')
-    assert_equal "0...", printer2.middle_line
-    printer3 = Printer.new('7A!')
-    assert_equal "00....00", printer3.middle_line
+    printer2 = Printer.new('FoSho!')
+    assert_equal "..0..0..0.00.000", printer2.middle_line
   end
 
   def test_can_return_bottom_line
     printer1 = Printer.new('ab')
     assert_equal "....", printer1.bottom_line
-    printer2 = Printer.new('2k')
-    assert_equal "..0.", printer2.bottom_line
-    printer3 = Printer.new('7A!')
-    assert_equal "...0..0.", printer3.bottom_line
-    assert_equal 8, printer3.bottom_line.length
+    printer2 = Printer.new('FoSho!')
+    assert_equal ".0..0..00...0.0.", printer2.bottom_line
+    assert_equal 16, printer2.bottom_line.length
   end
 
   def test_eighty_character_limit_per_line
@@ -69,6 +62,13 @@ class PrinterTest < Minitest::Test
     printer6 = Printer.new("Hello, world")
     expected3 = "..0.0.0.0.0......00.0.0.00\n..00.00.0..00...00.0000..0\n.0....0.0.0......00.0.0..."
     assert_equal expected3, printer6.translate_to_braille_max_eighty_chars
+  end
+
+  def test_can_translate_braille_to_english
+    printer = Printer.new([['0.', '..', '..'], ['0.', '0.', '..']])
+    assert_equal 'ab', printer.translate_braille_to_english
+    printer1 = Printer.new([['0.', '00', '..'], ['.0', '0.', '..'], ['..', '00', '0.']])
+    assert_equal 'hi!', printer1.translate_braille_to_english
   end
 
 end
