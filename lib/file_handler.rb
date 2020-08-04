ba6db9971170623ae2f85312bacec91ba4228899
fieldstyler <tdfields93@gmail.com>
2020-08-03 20:35:34 -0600

    Update to name arguments more clearly

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -1,21 +1,34 @@
+require "./lib/translator"
 module FileHandler
 
-  def self.read_and_write_file(input, output)
+  # def self.read_and_write_file(input, output)
+  #
+  #   contents = File.read(input)
+  #   File.open(output, "w") do |file|
+  #     file.write contents
+  #   end
+  #   "Created #{output} containing #{File.read(output).chomp.length} characters."
+  # end
 
-    contents = File.read(input)
-    File.open(output, "w") do |file|
-      file.write contents
+  def self.translate_to_braille(english_file_name, braille_file_name)
+
+    contents = File.read(english_file_name)
+    translator = Translator.new(contents)
+    File.open(braille_file_name, "w") do |file|
+      file.write translator.translate_to_braille_max_eighty_chars
     end
-    "Created #{output} containing #{File.read(output).chomp.length} characters."
+    "Created #{braille_file_name} containing #{File.read(braille_file_name).chomp.length} characters."
   end
 
-  def self.read_and_translate_file(input, output)
+  def self.translate_to_english(braille_file_name, english_file_name)
 
-    contents = File.read(input)
-    printer = Printer.new(contents)
-    File.open(output, "w") do |file|
-      file.write printer.translate_to_braille_max_eighty_chars
+    contents = File.read(braille_file_name)
+    translator = Translator.new(contents)
+    File.open(english_file_name, "w") do |file|
+      file.write translator.translate_braille_to_english
     end
+    "Created #{english_file_name} containing #{File.read(english_file_name).chomp.length} characters."
   end
+  #make translator module
 
 end
