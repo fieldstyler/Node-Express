eb22f47f4353328171dc33628720b7ba811ba2b4
fieldstyler <tdfields93@gmail.com>
2020-08-03 21:45:01 -0600

    Add puts before my return values to return correct information

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -8,7 +8,7 @@ module FileHandler
     File.open(braille_file_name, "w") do |file|
       file.write translator.translate_to_braille_max_eighty_chars
     end
-    "Created #{braille_file_name} containing #{File.read(braille_file_name).chomp.length} characters."
+    puts "Created #{braille_file_name} containing #{File.read(braille_file_name).chomp.length} characters."
   end
 
   def self.translate_to_english(braille_file_name, english_file_name)
@@ -18,7 +18,7 @@ module FileHandler
     File.open(english_file_name, "w") do |file|
       file.write translator.translate_braille_to_english
     end
-    "Created #{english_file_name} containing #{File.read(english_file_name).chomp.length} characters."
+    puts "Created #{english_file_name} containing #{File.read(english_file_name).chomp.length} characters."
   end
 
 end
